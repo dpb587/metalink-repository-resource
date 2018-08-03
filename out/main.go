dpb587/metalink-repository-resource

commit 8481d68c5d37c4e89c752eb346fc89232a4f0dd7

Add "rename_from_file"
This allows the metalink filename to be passed in via an input resource,
which enables the metalink filename to be dynamically generated.

Co-authored-by: Josh Russett <jrussett@pivotal.io>

out/main.go
@@ -87,6 +87,13 @@ func main() {
 
 	if request.Params.Rename != "" {
 		metalinkName = request.Params.Rename
+	} else if request.Params.RenameFromFile != "" {
+		var metalinkNameBytes []byte
+		metalinkNameBytes, err = ioutil.ReadFile(metalinkPath)
+		if err != nil {
+			api.Fatal("out: could not open rename file: read error", err)
+		}
+		metalinkName = string(metalinkNameBytes)
 	} else {
 		metalinkName = fmt.Sprintf("v%s.meta4", meta4.Files[0].Version)
 	}

out/api.go
@@ -10,11 +10,12 @@ type Request struct {
 }
 
 type Params struct {
-	Metalink string                 `json:"metalink"`
-	Files    []string               `json:"files"`
-	Version  string                 `json:"version"`
-	Rename   string                 `json:"rename,omitempty"`
-	Options  map[string]interface{} `json:"options,omitempty"`
+	Metalink       string                 `json:"metalink"`
+	Files          []string               `json:"files"`
+	Version        string                 `json:"version"`
+	Rename         string                 `json:"rename,omitempty"`
+	RenameFromFile string                 `json:"rename_from_file,omitempty"`
+	Options        map[string]interface{} `json:"options,omitempty"`
 }
 
 type Response struct {

commit 27e9abce3841a7ed84eb41e4f5304748c796a806

Fix rename_from_file
Broken since 8481d68c5d37c4e89c752eb346fc89232a4f0dd7

out/main.go
@@ -89,7 +89,7 @@ func main() {
 		metalinkName = request.Params.Rename
 	} else if request.Params.RenameFromFile != "" {
 		var metalinkNameBytes []byte
-		metalinkNameBytes, err = ioutil.ReadFile(metalinkPath)
+		metalinkNameBytes, err = ioutil.ReadFile(request.Params.RenameFromFile)
 		if err != nil {
 			api.Fatal("out: could not open rename file: read error", err)
 		}